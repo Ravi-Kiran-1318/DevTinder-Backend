de8068dad2eeb84f76a6ce8d8965007c5f634d2a
Ravi-Kiran-1318 <rkgit7767@gmail.com>
2026-02-10 19:33:38 +0530

    Error-handling

--- app.js
+++ app.js
@@ -1,38 +1,22 @@
 const express=require('express');
 const app=express();
 
-const {adminAuth, userAuth}=require('./middlewares/auth');
 
-app.use("/admin", adminAuth);
-
-app.use("/user", userAuth);
-
-app.get("/admin/dashboard",(req,res)=>{
-    res.send("Welcome to the admin dashboard");
+app.get("/getUserDetails",(req,res)=>{
+    try{
+        //Logic to get user details and get user data
+        throw new Error("Failed to fetch user details");
+        res.send("User details fetched successfully");
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }  
 })
 
-app.get("/admin/settings",(req,res)=>{
-    res.send("Welcome to the admin settings");
+app.use("/", (err, req, res, next)=>{
+    console.error(err.stack);
+    res.status(500).send("Something went wrong!");
 })
 
-app.get("/admin/getAllData",(req,res)=>{
-    res.send("Here is all the data");
-})
-
-app.get("/user", adminAuth, (req,res)=>{
-    //saving data to db
-    res.send("User created successfully");
-})
-
-app.post("/user/login", userAuth, (req,res)=>{
-    //saving data to db
-    res.send("User LoggedIn successfully");
-})
-
-
-app.get("/user/:userId",(req,res)=>{
-    console.log(req.params)
-    res.send({firstname:"Ravi",lastname:"Kiran",age:21});})  
     
 
 app.listen(7777,()=>{
